bf09c1cf303db00c647390f8e6989af74069a599
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-16 13:02:28 +0300

    fix linters issues

--- src/setup.js
+++ src/setup.js
@@ -1,27 +1,3 @@
-// const boards = [
-//   {
-//     name: 'Name',
-//     score: 100,
-//   },
-//   {
-//     name: 'Name',
-//     score: 360,
-//   },
-//   {
-//     name: 'Name',
-//     score: 460,
-//   },
-//   {
-//     name: 'Name',
-//     score: 1000,
-//   },
-// ];
-// boards.forEach((board) => {
-//   const lists = document.querySelector('.list');
-//   lists.innerHTML += `<li>${board.name}: ${board.score}</li>`;
-// });
-
-// localStorage.setItem('names and scores', JSON.stringify(boards));
 const refresh = document.querySelector('.refresh');
 const submit = document.querySelector('.submit');
 
